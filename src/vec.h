b2339f3a2a40dcc812cf40c95bb572ec1955f521
Nicolas Baumgardt <nicolas@baumgardt.ch>
2020-03-11 23:22:52 +0100

    Issue with vec_push() and vec_insert() fixed
    Discussion about the details of the issue can be found here: https://stackoverflow.com/questions/60643222/how-to-solve-right-hand-operand-has-no-effect-error

--- src/vec.h
+++ src/vec.h
@@ -33,7 +33,7 @@
 
 #define vec_push(v, val)\
   ( vec_expand_(vec_unpack_(v)) ? -1 :\
-    ((v)->data[(v)->length++] = (val), 0), 0 )
+    ((v)->data[(v)->length++] = (val), 0))
 
 
 #define vec_pop(v)\
@@ -52,7 +52,7 @@
 
 #define vec_insert(v, idx, val)\
   ( vec_insert_(vec_unpack_(v), idx) ? -1 :\
-    ((v)->data[idx] = (val), 0), (v)->length++, 0 )
+    ((v)->data[idx] = (val)), (v)->length++, 0 )
     
 
 #define vec_sort(v, fn)\
